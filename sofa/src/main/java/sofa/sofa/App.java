a46802299a62ba09902685aff17c583352994a45
Gabriel Sanz Salas <gsa@systems.bz>
2018-11-05 15:44:29 +0100

    added sofa

---
+++ sofa/src/main/java/sofa/sofa/App.java
@@ -0,0 +1,21 @@
+package sofa.sofa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static int add(int a, int b) {
+    	return a+b;
+    }
+    
+    public static int mult(int a, int b) {
+    	return a*b;
+    }
+}
